f9904b180aaf034e0c50a31d0d2610b21b0d0b7e
Deepak Kumar <deepak.s.kumar.85@gmail.com>
2016-04-28 13:18:51 -0400

    new firmware and other ui things

--- photon-files/powermonitor.ino
+++ photon-files/powermonitor.ino
@@ -1,19 +1,24 @@
 #include "math.h"
 int analogvals[2500];
+double a[3] = {0.008732,    0.008732*2,    0.008732};
+double b[2] = {-1.7188,    0.7537};
+double prev[2];
+double filtered[2];
 int cnt,cnt2;
-double Vmid = 1.61;
-double avgsamp, RMScurrent;
+double n_avg = 10;
+double Vmid = 1.613;
+double voltage;
+double RMSsamp, RMScurrent, RMSwattage;
 char publishString[40];
 
 void setup() {
-    Particle.variable("RMScurrent", &RMScurrent, DOUBLE); 
+    Particle.variable("RMSwattage", &RMSwattage, DOUBLE); 
 }
 
 void loop() {
     
     //very quickly sample the signal
     cnt = 0;
-    
     while(cnt<2500){
         
         analogvals[cnt] = analogRead(A0); ///store current read to the array location cnt
@@ -21,29 +26,51 @@ void loop() {
         
     }
     
-    //Do transform the raw readings to squared current values
+    //transform the raw readings into averages and then calculate RMS current
     cnt = 0;
     RMScurrent = 0;
-    
-    
     while(cnt<2500){
-        
+
         //create a averaged point of 10 samples
+        RMSsamp = 0;
         cnt2 = 0;
-        avgsamp = 0;
-        while (cnt2<10){
-            avgsamp = avgsamp + (double) analogvals[cnt];
+        while (cnt2<n_avg && cnt < 2500){
+            //convert the raw readings to a voltage
+            voltage = (double)analogvals[cnt]*3.3/4095 - Vmid;
+            
+            //implement a 2nd order butterworth to filter the raw readings
+            filtered[0] = a[0]*voltage + a[1]*prev[0] + a[2]*prev[1] - b[0]*filtered[0] - b[1]*filtered[1];
+            prev[1] = prev[0];
+            prev[0] = voltage;
+            filtered[1] = filtered[0];
+            
+            //convert the filtered value and square it, add it to the sum of 10 RMS values
+            RMSsamp = RMSsamp + pow(filtered[0]*20,2);
             cnt = cnt + 1;
             cnt2 = cnt2 + 1;
         }
         
-        RMScurrent = RMScurrent + pow((3.3*avgsamp/4095-Vmid)*20,2);//convert to a current, square it, and sum
+        //average 10 RMS values
+        RMSsamp = RMSsamp/n_avg;
+        
+        //sum the squared and averaged samples
+        RMScurrent = RMScurrent + RMSsamp;
+        
     }
     
-    RMScurrent = sqrt(RMScurrent/250);//root mean of 2500/10 = 250 averaged samples
+    //find the total RMS current value of the entire data set
+    RMScurrent = sqrt(RMScurrent/(2500/n_avg));
+    RMSwattage = 120*(1.44*RMScurrent-0.25);
     
-    sprintf(publishString, "%lf", RMScurrent);
-    Particle.publish("RMScurrent",publishString);
+    
+    //Particle.publish("keen_io_wattage","{ \"1\": \"" + String(RMSwattage) + "\", 60, PRIVATE);
+    Particle.publish("keen_io_wattage", "{ \"w\": \"" + String(RMSwattage) + "\" }", 60, PRIVATE);
+    //publish the RMS wattage value with a linear calibration
+    //sprintf(publishString, "%lf", RMSwattage);
+    //Particle.publish("thingSpeakWrite_A0", "{ \"1\": \"" + String(value) + "\", \"k\": \"XXXXXXXXXXXXXXXX\" }", 60, PRIVATE);
+    //Particle.publish("keen_io_RMSwattage", publishString);
+    //Particle.publish("custom_templates", "{ \"my-var\": \"foo\", \"my-temp\": \"98.6F\" }", 60, PRIVATE);
+    //Particle.publish("keen_io_RMSwattage", publishString);
     delay(1000);
  
 }
